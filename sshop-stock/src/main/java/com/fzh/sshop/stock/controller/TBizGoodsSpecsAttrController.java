493cc7a96ec782c28207a81f7ac609499440c80f
jika001 <jika001>
2020-06-23 18:19:08 +0800

    init

--- sshop-stock/src/main/java/com/fzh/sshop/stock/controller/TBizGoodsSpecsAttrController.java
+++ sshop-stock/src/main/java/com/fzh/sshop/stock/controller/TBizGoodsSpecsAttrController.java
@@ -1,9 +1,21 @@
 package com.fzh.sshop.stock.controller;
 
 
+import com.fzh.sshop.request.SuperResponse;
+import com.fzh.sshop.stock.req.*;
+import com.fzh.sshop.stock.service.impl.TBizBrandServiceImpl;
+import com.fzh.sshop.stock.service.impl.TBizGoodsServiceImpl;
+import com.fzh.sshop.stock.service.impl.TBizGoodsSpecsAttrServiceImpl;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 /**
  * <p>
@@ -15,7 +27,39 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/stock/goodsSpecsAttr")
+@Api(value = "后台管理",tags = {"admin-商品属性管理"})
 public class TBizGoodsSpecsAttrController {
+    @Autowired
+    private TBizGoodsSpecsAttrServiceImpl service;
 
+    @PostMapping("/list")
+    @ApiOperation(value = "商品属性列表" ,notes = "")
+    public SuperResponse list(@Valid @RequestBody GoodsSpecsAttrListRequest request ){
+        return  service.list(request);
+    }
+
+    @PostMapping("/find")
+    @ApiOperation(value = "商品属性详情" ,notes = "")
+    public SuperResponse find(@Valid @RequestBody GoodsSpecsAttrRequest request ){
+        return service.find(request);
+    }
+
+    @PostMapping("/insert")
+    @ApiOperation(value = "商品属性添加" ,notes = "")
+    public SuperResponse delete(@Valid @RequestBody GoodsSpecsAttrInfoRequest  request){
+        return service.insert(request);
+    }
+
+    @PostMapping("/update")
+    @ApiOperation(value = "商品属性修改" ,notes = "")
+    public SuperResponse update(@Valid @RequestBody GoodsSpecsAttrInfoRequest request){
+        return service.update(request);
+    }
+
+    @PostMapping("/delete")
+    @ApiOperation(value = "商品属性删除" ,notes = "")
+    public SuperResponse delete(@Valid @RequestBody GoodsSpecsAttrRequest request ){
+        return service.delete(request);
+    }
 }
 

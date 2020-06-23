9a2de5c46161b58a4f2bd64a46c7bf3855aa1d21
jika001 <jika001>
2020-06-23 15:19:16 +0800

    init

---
+++ sshop-payment/src/main/java/com/fzh/sshop/config/mybatis/MybatisPlusConfig.java
@@ -0,0 +1,49 @@
+package com.fzh.sshop.config.mybatis;
+
+import com.baomidou.mybatisplus.core.injector.ISqlInjector;
+import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.Properties;
+
+
+@EnableTransactionManagement
+@Configuration
+public class MybatisPlusConfig {
+
+    //TODO 分页插件
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+    //TODO 乐观锁插件
+    @Bean
+    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+        return new OptimisticLockerInterceptor();
+    }
+    //TODO 逻辑删除
+    @Bean
+    public ISqlInjector sqlInjector() {
+        return new LogicSqlInjector();
+    }
+
+    //TODO 打印SQL
+    @Bean
+    @Profile({"dev","test"})// 设置 dev test 环境开启，
+    public PerformanceInterceptor performanceInterceptor() {
+        PerformanceInterceptor performanceInterceptor =new PerformanceInterceptor();
+        performanceInterceptor.setMaxTime(500); //ms 设置sql执行的最大时间，如果超过了则不执行
+        performanceInterceptor.setFormat(true);
+        Properties properties = new Properties();
+        properties.setProperty("format", "false");
+        performanceInterceptor.setProperties(properties);
+        return performanceInterceptor;
+    }
+
+}
